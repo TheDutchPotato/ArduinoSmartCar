b7e5ec701344d9ad4eb679417b12ff2cc3b261e0
TheDutchPotato <griffin02@hotmail.nl>
2025-03-31 13:10:12 +0200

    first class and motor pinout
    created first class with pinout and reference in .cpp

--- Main/DeviceDriverSet_x.cpp
+++ Main/DeviceDriverSet_x.cpp
@@ -1,2 +1,5 @@
-//File for definition of device function for example motor control forward
+//File for definition of device function and setting variables
 #include "DeviceDriverSet_x.h"
+#include <avr/wdt.h>
+
+void DeviceDriverSet_Motor::
--- Main/DeviceDriverSet_x.h
+++ Main/DeviceDriverSet_x.h
@@ -1,5 +1,17 @@
-//Header file for classes definition and setting pinout/variables
+//Header file for classes definition and defining pinout/variables
 #ifndef _DeviceDriverSet_x_H_
 #define _DeviceDriverSet_x_H_
 
 #include <Arduino.h>
+
+class DeviceDriverSet_Motor
+{
+
+private:{
+  #define PIN_Motor_PWMA 5
+  #define PIN_Motor_PWMB 6 
+  #define PIN_Motor_BIN_1 8
+  #define PIN_Motor_AIN_1 7
+  #define PIN_Motor_STBY 3
+};
+}
