6a21e4d72400a9a3781c26e72b869cc41eda75e6
TheDutchPotato <griffin02@hotmail.nl>
2025-03-31 19:35:53 +0200

    Set base classes
    Setup the required basic classes and the initialize pinouts for various sensors

--- Main/DeviceDriverSet_x.cpp
+++ Main/DeviceDriverSet_x.cpp
@@ -1,5 +1,31 @@
+#include "Arduino.h"
 //File for definition of device function and setting variables
 #include "DeviceDriverSet_x.h"
 #include <avr/wdt.h>
 
-void DeviceDriverSet_Motor::
+void DeviceDriverSet_Motor::DeviceDriverSet_Motor_Init(void) //Set pins
+{
+  pinMode(PIN_Motor_PWM_L, OUTPUT);
+  pinMode(PIN_Motor_PWM_R, OUTPUT);
+  pinMode(PIN_Motor_LIN_1, OUTPUT);
+  pinMode(PIN_Motor_RIN_1, OUTPUT);
+  pinMode(PIN_Motor_STBY, OUTPUT);
+};
+
+void DeviceDriverSet_Ultrasonic::DeviceDriverSet_Ultrasonic_Init(void)
+{
+  pinMode(PIN_Echo, INPUT);
+  pinMode(PIN_Trigger, OUTPUT);
+};
+
+void DeviceDriverSet_LineTrack::DeviceDriverSet_Linetrack_Init(void)
+{
+  pinMode(PIN_LineTrack_L, INPUT);
+  pinMode(PIN_LineTrack_M, INPUT);
+  pinMode(PIN_LineTrack_R, INPUT);
+};
+
+void DeviceDriverSet_Servo::DeviceDriverSet_Servo_Init(/*unsigned int Position_Angle*/void)
+{
+
+};
