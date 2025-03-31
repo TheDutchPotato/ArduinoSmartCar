d8cf819b81a2e19a38e68510fb1f9a30c67efdc8
TheDutchPotato <griffin02@hotmail.nl>
2025-03-31 12:21:07 +0200

    Create Library files for Motor control
    Add library header and functions file for motor control and set references

---
+++ Main/MotorDriver.cpp
@@ -0,0 +1,2 @@
+// File to set functions for class MotorDriver.h
+#include "MotorDriver.h"
---
+++ Main/MotorDriver.h
@@ -0,0 +1,5 @@
+//Header file for class definition
+#ifndef _MotorDriver_H_
+#define _MotorDriver_H_
+
+#include <Arduino.h>
